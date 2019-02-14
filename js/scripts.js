d59464fdac0ef46b0eda50c77527cf80628090d6
amiinegal <you@example.com>
2019-02-14 12:00:31 +0300

    index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,40 @@
 var add = function(number1, number2) {
-  return number1 + number2;
+return number1 + number2;
 };
-
+//i looks good right
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
 
-var add = function(number1, number2) {
-  return number1 - number2;
+var subtract =function(number3, number4) {
+  return number3 - number4;
 };
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
+var number3 = parseInt(prompt("Enter a number"));
+var number4 = parseInt(prompt("Enter another number"));
+var result = subtract(number3, number4);
 alert(result);
 
-var add = function(number1, number2) {
-  return number1 - number2;
+var multiply =function(number5, number6) {
+  return number5 * number6;
 };
+var number5 = parseInt(prompt("Enter a number"));
+var number6 = parseInt(prompt("Enter another number"));
+var result = multiply(number5, number6);
+alert(result);
 
-var multiply = function(number1, number2) {
-  return number1 * number2;
+var divide =function(number7, number8) {
+  return number7 / number8;
 };
+var number7 = parseInt(prompt("Enter a number"));
+var number8 = parseInt(prompt("Enter another number"));
+var result = divide(number7, number8);
+alert(result);
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
+var divide = function(weight, height) {
+  return weight / (height*height/10000);
+};
+var w2 = parseInt(prompt("Enter your weight"));
+var h2 = parseInt(prompt("Enter your height"));
+alert(divide(w2, h2));
 alert(result);
